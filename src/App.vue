f161c0947d064b9c3b512e4c6a4115d226f42383
lahlaliwassim2 <wlahlali34@gmail.com>
2023-04-21 11:51:28 +0000

    ajouter les routes pricipeaux

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav>
+    <router-link  :class="{active: $route.name ==  'Home'}" to="/">Home</router-link> |
+    <router-link  :class="{active: $route.name == 'Cart'}" to="/cart">Cart</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+body {
+  background-color: rgb(245, 245, 245);
+}
+#nav{
+  padding: 10px;
+  width: 100%;
+  height: 30px;
+  border-color: aqua;
+  line-height: 30px;
+
+  a{
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+    margin: 0 5px 0 5px;
+    font-size: 1.25rem;
+
+    }
+  }
+
+
+.active {
+      color: blue;
+}
+.text-center {
+  text-align: center;
+}
+</style>
