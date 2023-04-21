974fbc209bcf6b146d40d3701dace1f94b465b87
lahlaliwassim2 <wlahlali34@gmail.com>
2023-04-21 11:52:34 +0000

    ajoute les données pour le test

---
+++ src/data/items.js
@@ -0,0 +1,52 @@
+const items = [
+    {
+        id: 1,
+        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        name: "banane",
+        price: 12.00,
+        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+    },
+    {
+        id: 2,
+        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        name: "tofa7",
+        price: 12.00,
+        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+    },
+    {
+        id: 3,
+        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        name: "baouid",
+        price: 12.00,
+        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+    },
+    {
+        id: 4,
+        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        name: "na3na3",
+        price: 12.00,
+        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+    },
+    {
+        id: 5,
+        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        name: "dala7",
+        price: 12.00,
+        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+    },
+    {
+        id: 6,
+        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        name: "avoca",
+        price: 12.00,
+        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+    },
+    {
+        id: 7,
+        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        name: "limon",
+        price: 12.00,
+        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+    },
+]
+export default items ; 
