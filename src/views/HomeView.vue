f94c318433c25461c57620759bde478f603e8e7e
lahlaliwassim2 <wlahlali34@gmail.com>
2023-04-21 11:52:57 +0000

    ajouter la partie de produit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+   
+    <div class="product-cards-container">
+      <ProductSummarayCard
+       v-for="product in items"
+      :key="product.id"
+      :product= "product"
+       />
+    </div>
+  </div>
+</template>
+
+<script>
+import items from '../data/items.js'
+import ProductSummarayCard from '../components/products/ProductSummarayCard.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    ProductSummarayCard
+  },
+  data () {
+    return {
+      items: items
+    }
+  }
+}
+</script>
+<style lang="scss">
+.product-cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+}
+
+</style>
