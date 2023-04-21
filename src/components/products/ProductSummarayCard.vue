9a6eb7cacb2526ad6ff97805d0d3362a0625d812
lahlaliwassim2 <wlahlali34@gmail.com>
2023-04-21 11:51:52 +0000

    ajouter les composants de produits

---
+++ src/components/products/ProductSummarayCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="card">
+        <h3>{{product.name}}</h3>
+        <h5 class="price">Prix est : ${{ product.price}}</h5>
+        <p class="description"> </p>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['product']
+    }
+</script>
