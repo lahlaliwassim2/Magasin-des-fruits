101f6670a3d9d06367dafd124e75da094cad4254
lahlaliwassim2 <wlahlali34@gmail.com>
2023-04-26 09:13:32 +0100

    Separer la 'Navbar' dans un seul composant

---
+++ src/components/NavbarView.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <router-link :class="{active: $route.name == 'Home'}" to="/">MAGASIN</router-link> 
+    <router-link :class="{active: $route.name == 'Cart'}" to="/cart">
+      <i class="bi bi-cart-fill"></i>
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavbarView'
+}
+</script>
+
+<style>
+nav {
+  padding: 10px;
+  width: 100%;
+  height: 30px;
+  border-color: aqua;
+  line-height: 30px;
+  display: flex;
+  justify-content: space-around;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  margin: 0 5px 0 5px;
+  font-size: 1.25rem;
+}
+
+.active {
+  color: blue;
+}
+</style>
