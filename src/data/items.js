90b7ebe4cd3efcd287b0f6236010d77275a4e21e
lahlaliwassim2 <wlahlali34@gmail.com>
2023-04-26 09:13:53 +0100

    modification de l objet data

--- src/data/items.js
+++ src/data/items.js
@@ -1,53 +1,53 @@
-const items = [
+const items =[
     {
         id: 1,
-        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        name: "banane",
-        price: 12.00,
-        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+        image: "https://www.foodette.fr/wp-content/uploads/2017/08/apple-417924_1920-compressor-1024x656.jpg",
+        name: "Pomme",
+        price: 2.50,
+        descrption: "Une pomme croquante et juteuse, parfaite pour une collation ou une recette de tarte."
     },
     {
         id: 2,
-        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        name: "tofa7",
-        price: 12.00,
-        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+        image: "https://www.foodette.fr/wp-content/uploads/2017/08/apple-417924_1920-compressor-1024x656.jpg",
+        name: "Orange",
+        price: 1.80,
+        descrption: "Une orange juteuse pleine de vitamine C pour vous donner un coup de fouet énergétique."
     },
     {
         id: 3,
-        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        name: "baouid",
-        price: 12.00,
-        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+        image: "https://www.foodette.fr/wp-content/uploads/2017/08/apple-417924_1920-compressor-1024x656.jpg",
+        name: "Banane",
+        price: 2.00,
+        descrption: "Une banane douce et crémeuse, pleine de potassium pour une santé optimale."
     },
     {
         id: 4,
-        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        name: "na3na3",
-        price: 12.00,
-        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+        image: "https://www.foodette.fr/wp-content/uploads/2017/08/apple-417924_1920-compressor-1024x656.jpg",
+        name: "Ananas",
+        price: 3.50,
+        descrption: "Un ananas tropical frais et sucré, parfait pour ajouter une touche exotique à vos plats."
     },
     {
         id: 5,
-        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        name: "dala7",
-        price: 12.00,
-        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+        image: "https://www.foodette.fr/wp-content/uploads/2017/08/apple-417924_1920-compressor-1024x656.jpg",
+        name: "Fraise",
+        price: 4.00,
+        descrption: "Des fraises fraîches et sucrées, idéales pour ajouter une touche de couleur à vos desserts."
     },
     {
         id: 6,
-        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        name: "avoca",
-        price: 12.00,
-        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
+        image: "https://www.foodette.fr/wp-content/uploads/2017/08/apple-417924_1920-compressor-1024x656.jpg",
+        name: "Fruit de la passion",
+        price: 2.80,
+        descrption: "Un fruit de la passion acidulé et aromatique, parfait pour ajouter une touche d'audace à vos boissons."
     },
     {
         id: 7,
-        image: "https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        name: "limon",
-        price: 12.00,
-        descrption: "azerty dzjdhz kkkd hdy zdlj yd grre dhd jdzoj ydzih dhzd"
-    },
+        image: "https://www.foodette.fr/wp-content/uploads/2017/08/apple-417924_1920-compressor-1024x656.jpg",
+        name: "Mangue",
+        price: 3.00,
+        descrption: "Une mangue douce et juteuse, parfaite pour ajouter une touche tropicale à vos recettes de salade."
+    }
 ]
 export default items ; 
 
