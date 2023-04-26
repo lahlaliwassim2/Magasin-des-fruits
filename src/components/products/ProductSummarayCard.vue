d7fcb69291d3a7a3ac419de371ec6fc2abff818a
lahlaliwassim2 <wlahlali34@gmail.com>
2023-04-26 09:39:06 +0100

    boucler sur l objet items pour affichier les produits existent

--- src/components/products/ProductSummarayCard.vue
+++ src/components/products/ProductSummarayCard.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="card">
-        <img :src="image" alt="">
-        <h3>{{product.name}}</h3>
-        <h5 class="price">Prix est : {{ product.price}}</h5>
-        <p class="description">{{ description }}</p>
-        <p class="text-muted"> {{ product.category }}</p>
-        <button  class="view-product-button" @click="$emit('view-product', product)">view</button>
-   </div>
+    <button class="view-product-button" @click="$emit('view-product', product)">
+        <div class="card bg-gray-900">
+            <h3>{{product.name}}</h3>
+            <img :src="image" alt="">
+            <h5 class="price">Prix est : {{ product.price}}</h5>
+            <p class="description">{{ description }}</p>
+            <p class="text-muted"> {{ product.category }}</p>
+        </div>
+    </button>
 </template>
+
 <script>
     export default {
         props: ['product'],
-       
+
         computed: {
             description() {
                 return this.product.descrption.substring(0, 150)
@@ -22,44 +24,48 @@
         }
     }
 </script>
-<style lang="scss">
-    .card{
+
+<style>
+    .card {
         width: 80%;
         margin: 10%;
         padding: 10px;
         border-radius: 5px;
         background-color: #fff;
         box-shadow: 0 5px gray;
-        img {
-            width: 104%;
-            height: 60%;
-            }
-        h5.price{
-            color: gray;
-
-        }
-        p.description{
-            font-size: .85rem;
-        }
-        p.text-muted{
-            color: gray;
-        }
-        button.view-product-button{
-            padding: 10px;
-            background-color: rgb(79, 160, 187);
-            border: none;
-            color: wheat;
-            font-weight: bold;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
     }
-    @media (min-width: 500px ) {
-        .card{
+
+    .card img {
+        width: 104%;
+        height: 60%;
+    }
+
+    .card h5.price {
+        color: gray;
+    }
+
+    .card p.description {
+        font-size: .85rem;
+    }
+
+    .card p.text-muted {
+        color: gray;
+    }
+
+    button.view-product-button {
+        padding: 10px;
+        background-color: rgb(79, 160, 187);
+        border: none;
+        color: wheat;
+        font-weight: bold;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    @media (min-width: 500px) {
+        .card {
             width: 350px;
             margin: 10px;
-        }        
+        }
     }
-
 </style>
