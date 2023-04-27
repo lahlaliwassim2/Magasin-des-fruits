fb43e3d749ccafd91ae55cf50ccdaf6165164c93
lahlaliwassim2 <wlahlali343@gmail.com>
2023-04-27 10:17:34 +0000

    ajouter le style pour le composant ProductSummarayCard

--- src/components/products/ProductSummarayCard.vue
+++ src/components/products/ProductSummarayCard.vue
@@ -1,70 +1,114 @@
 <template>
-    <button class="view-product-button" @click="$emit('view-product', product)">
-        <div class="card bg-gray-900">
-            <h3>{{product.name}}</h3>
-            <img :src="image" alt="">
-            <h5 class="price">Prix est : {{ product.price}}</h5>
+    <!-- <button class="view-product-button" @click="$emit('view-product', product)">
+        <div class="card ">
+            <h3 class="text-muted bg-gray-600">{{ product.name }}</h3>
+            <img class="card-img" :src="image" alt="">
+            <h5 class="price">Prix est : {{ product.price }}</h5>
             <p class="description">{{ description }}</p>
-            <p class="text-muted"> {{ product.category }}</p>
         </div>
-    </button>
+    </button> -->
+    <div class="product-card">
+		
+		<div class="product-tumb">
+			<img :src="image" alt="" style="width:100%;">
+		</div>
+		<div class="product-details">
+			<h4><a href="">{{ product.name }}</a></h4>
+			<p>{{ description }}</p>
+			<div class="product-bottom-details">
+				<div class="product-price">{{ product.price }} dh</div>	
+                <button @click="$emit('view-product', product)">View</button>	
+			</div>
+		</div>
+	</div>
+
 </template>
 
 <script>
-    export default {
-        props: ['product'],
+export default {
+    props: ['product'],
 
-        computed: {
-            description() {
-                return this.product.descrption.substring(0, 150)
-            },
-            image() {
-                return this.product.image
-            }
+    computed: {
+        description() {
+            return this.product.descrption.substring(0, 150)
+        },
+        image() {
+            return this.product.image
         }
     }
+}
 </script>
 
 <style>
+
+a
+{
+    text-decoration: none;
+}
+.product-card {
+    width: 380px;
+    position: relative;
+    box-shadow: 0 2px 7px #dfdfdf;
+    margin: 50px auto;
+    background: #fafafa;
+}
+
+
+.product-tumb {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 300px;
+    padding: 50px;
+    background: #f0f0f0;
+}
+
+.product-tumb img {
+    max-width: 100%;
+    max-height: 100%;
+}
+
+.product-details {
+    padding: 30px;
+}
+.product-details h4 a {
+    font-weight: 500;
+    display: block;
+    margin-bottom: 18px;
+    text-transform: uppercase;
+    color: #363636;
+    text-decoration: none;
+    transition: 0.3s;
+}
+
+.product-details h4 a:hover {
+    color: #fbb72c;
+}
+
+.product-details p {
+    font-size: 15px;
+    line-height: 22px;
+    margin-bottom: 18px;
+    color: #999;
+}
+
+.product-bottom-details {
+    overflow: hidden;
+    border-top: 1px solid #eee;
+    padding-top: 20px;
+}
+
+.product-bottom-details div {
+    float: left;
+    width: 50%;
+}
+
+
+
+@media (min-width: 500px) {
     .card {
-        width: 80%;
-        margin: 10%;
-        padding: 10px;
-        border-radius: 5px;
-        background-color: #fff;
-        box-shadow: 0 5px gray;
+        width: 350px;
+        margin: 10px;
     }
-
-    .card img {
-        width: 100%;
-        height: 60%;
-    }
-
-    .card h5.price {
-        color: gray;
-    }
-
-    .card p.description {
-        font-size: .85rem;
-    }
-
-    .card p.text-muted {
-        color: gray;
-    }
-
-    button.view-product-button {
-        padding: 10px;
-        border: none;
-        color: wheat;
-        font-weight: bold;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-
-    @media (min-width: 500px) {
-        .card {
-            width: 350px;
-            margin: 10px;
-        }
-    }
+}
 </style>
