4885bb558418d0900ba7d70ea82a55e8e8f53672
lahlaliwassim2 <wlahlali343@gmail.com>
2023-04-27 10:35:28 +0000

    ajouter de style pour le composant ProductDesription

--- src/components/products/ProductDescription.vue
+++ src/components/products/ProductDescription.vue
@@ -57,76 +57,110 @@ export default {
 }
 </script>
 <style>
-.drawer-background {
-    width: 100%;
-    height: 100vh;
-    position: fixed;
-    left: 0;
-    top: 0;
-    background-color: rgba(134, 124, 124, 0.55);
-    z-index: 100;
-    display: none;
-    transition: display .5s;
-}
+    .drawer-background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.6);
+        z-index: 999;
+        opacity: 0;
+        visibility: hidden;
+        transition: all 0.3s ease-in-out;
+    }
 
-.drawer-background.show {
-    display: block;
-}
-.flex {
-    display: flex;
-    justify-content: space-between;
-}
-.drawer {
-    width: 95vw;
-    height: 100vh;
-    background-color: #fff;
-    position: fixed;
-    top: 0%;
-    visibility: hidden;
-    padding: 15px;
-    transition: left .5s;
-    z-index: 101;
-    overflow: scroll;
-}
+    .drawer-background.show {
+        opacity: 1;
+        visibility: visible;
+    }
 
-.drawer.show {
-    visibility: visible;
-}
+    .drawer {
+        position: fixed;
+        top: 0;
+        right: 0;
+        height: 100%;
+        width: 400px;
+        background-color: #fff;
+        z-index: 1000;
+        transform: translateX(100%);
+        transition: all 0.3s ease-in-out;
+        overflow-y: auto;
+    }
 
-.drawer-close {
-    font-size: 1.5rem;
-    padding: 5px;
-    border-radius: 5px;
-    right: 10px;
-    border: 2px solid gray;
-    width: 15px;
-    float: right;
-    cursor: pointer;
-}
+    .drawer.show {
+        transform: translateX(0%);
+    }
 
-.drawer-close.hover {
-    background-color: lightcoral;
-}
+    .drawer-close {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        font-size: 1.2rem;
+        cursor: pointer;
+    }
 
-.product-details {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-}
+    .product-details {
+        padding: 20px;
+    }
 
-.product-details p.description {
-    padding: 20px;
-    line-height: 1.51rem;
-}
+    .product-details img {
+        width: 100%;
+        height: auto;
+        margin-bottom: 20px;
+    }
 
-.button-container button {
-    width: 150px;
-    border: none;
-    padding: 10px;
-    border-radius: 5px;
-    margin: 0 5px 50px 5px;
-    cursor: pointer;
-}
+    .product-details h3 {
+        font-size: 1.5rem;
+        margin-bottom: 10px;
+    }
+
+    .description {
+        font-size: 1rem;
+        margin-bottom: 20px;
+    }
+
+    .cart-total h4, .prix-final h4 {
+        font-size: 1.2rem;
+        margin-bottom: 10px;
+    }
+
+    .button-container {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+    }
+
+    .button-container button {
+        width: 30px;
+        height: 30px;
+        border: none;
+        background-color: #eee;
+        cursor: pointer;
+        margin: 0 5px;
+        font-size: 1.2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .button-container button:hover {
+        background-color: #ddd;
+    }
+
+    .remove {
+        color: #ff6961;
+    }
+
+    .add {
+        color: #00b894;
+    }
+
+    .flex {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 
 @media (min-width: 500px) {
     .drawer {
