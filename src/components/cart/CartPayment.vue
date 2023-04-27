0aa19d92c4fd3a564cf567af9a1253ed5bbfedfd
lahlaliwassim2 <wlahlali343@gmail.com>
2023-04-27 13:31:27 +0000

    styliser le composant 'CartPayement't

--- src/components/cart/CartPayment.vue
+++ src/components/cart/CartPayment.vue
@@ -1,16 +1,31 @@
 <template>
-    <div class="bg-gray-900">
-        <div>le prix que vous devais payées : </div>
-        <div>{{test}}</div>
+    <div class="division">
+        <div>Le prix total : </div>
+        <div>{{prix_total}} DH</div>
     </div>
 </template>
 <script>
 export default {
   props: ['product'],
     computed: {
-      test() {
+      prix_total() {
         return  this.$store.getters.SomePrixTotal
       }
     }
 }
 </script>
+
+<style>
+  .division {
+        background-color: #eb8755;
+        color: #1f0e0e;
+        padding: 10px;
+        font-size: 20px;
+        font-weight: bold;
+        text-align: center;
+        margin-top: 126px;
+    }
+    div:first-child {
+        margin-bottom: 10px;
+    }
+</style>
