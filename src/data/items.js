31dca795c4cb13c584f781d9fd650369c98f359d
lahlaliwassim2 <wlahlali343@gmail.com>
2023-04-27 16:41:11 +0000

    ajouter les données

--- src/data/items.js
+++ src/data/items.js
@@ -1,35 +1,36 @@
-const items =[
+const items = [
     {
         id: 1,
-        image: "https://www.shutterstock.com/image-photo/green-red-apple-small-wagon-260nw-129107447.jpg",
-        name: "Pomme",
-        price: 2.50,
-        descrption: "Une pomme croquante et juteuse, parfaite pour une collation ou une recette de tarte."
-    },
+        image: "https://www.conservation-nature.fr/wp-content/uploads/visuel/fruit/shutterstock_770858806-260x195.jpg",
+        name: "Abricot",
+        price: 13.00,
+        descrption: "Ce fruit délicat est issu de la Chine et possède un noyau central. Il y en a plus de quarante variétés cultivées généralement en Turquie."
+    }
+    ,
     {
         id: 2,
-        image: "https://www.shutterstock.com/image-photo/orange-fruit-slices-leaves-isolated-260nw-1386912362.jpg",
+        image: "https://www.conservation-nature.fr/wp-content/uploads/visuel/fruit/shutterstock_342874121-260x195.jpg",
         name: "Orange",
         price: 1.80,
         descrption: "Une orange juteuse pleine de vitamine C pour vous donner un coup de fouet énergétique."
     },
     {
         id: 3,
-        image: "https://www.foodette.fr/wp-content/uploads/2017/08/apple-417924_1920-compressor-1024x656.jpg",
+        image: "https://www.conservation-nature.fr/wp-content/uploads/visuel/fruit/shutterstock_575528746-260x195.jpg",
         name: "Banane",
         price: 2.00,
         descrption: "Une banane douce et crémeuse, pleine de potassium pour une santé optimale."
     },
     {
         id: 4,
-        image: "https://www.foodette.fr/wp-content/uploads/2017/08/apple-417924_1920-compressor-1024x656.jpg",
+        image: "https://www.conservation-nature.fr/wp-content/uploads/visuel/fruit/shutterstock_186916061-260x195.jpg",
         name: "Ananas",
         price: 3.50,
         descrption: "Un ananas tropical frais et sucré, parfait pour ajouter une touche exotique à vos plats."
     },
     {
         id: 5,
-        image: "https://www.shutterstock.com/image-photo/apple-tower-on-wooden-wagon-260nw-2209548281.jpg",
+        image: "https://www.conservation-nature.fr/wp-content/uploads/visuel/fruit/shutterstock_192308075-260x195.jpg",
         name: "Fraise",
         price: 4.00,
         descrption: "Des fraises fraîches et sucrées, idéales pour ajouter une touche de couleur à vos desserts."
@@ -41,13 +42,35 @@ const items =[
         price: 2.80,
         descrption: "Un fruit de la passion acidulé et aromatique, parfait pour ajouter une touche d'audace à vos boissons."
     },
+
     {
         id: 7,
-        image: "https://www.shutterstock.com/image-photo/row-strawberries-smallest-largest-on-260nw-1740128438.jpg",
-        name: "Mangue",
-        price: 3.00,
-        descrption: "Une mangue douce et juteuse, parfaite pour ajouter une touche tropicale à vos recettes de salade."
-    }
+        image: "https://www.conservation-nature.fr/wp-content/uploads/visuel/fruit/shutterstock_575378506-260x195.jpg",
+        name: "Pomme",
+        price: 2.50,
+        descrption: "Une pomme croquante et juteuse, parfaite pour une collation ou une recette de tarte."
+    },
+    {
+        id: 8,
+        image: "https://www.conservation-nature.fr/wp-content/uploads/visuel/fruit/shutterstock_1058981363-260x195.jpg",
+        name: "Avocat",
+        price: 12.50,
+        descrption: "L’avocat est le fruit d’un arbre à fleur (Persea Americana) de la famille des Lauraceae, originaire d’Amérique centrale."
+    },
+    {
+        id: 9,
+        image: "https://www.conservation-nature.fr/wp-content/uploads/visuel/fruit/shutterstock_739171414-260x195.jpg",
+        name: "Cerise",
+        price: 18.50,
+        descrption: "La cerise est le fruit de plusieurs variétés d’arbres de la famille des Rosaceae regroupés sous le nom générique de Prunus. Il existe plusieurs espèces de cerisiers originaires d’Europe, d’Amérique du Nord et d’Asie. Ce sont des arbres robustes qui demandent du soleil et de la place pour se développer."
+    },
+    {
+        id: 10,
+        image: "https://www.conservation-nature.fr/wp-content/uploads/visuel/fruit/shutterstock_1470172706-260x195.jpg",
+        name: "Kiwi",
+        price: 18.50,
+        descrption: "Le kiwi est le fruit d’une plante grimpante ligneuse de la famille des Actinidiaceae originaire de Chine. Actuellement cultivé un peu partout à travers le monde, son principal producteur reste la Chine.."
+    },
 ]
-export default items ; 
+export default items;
 
