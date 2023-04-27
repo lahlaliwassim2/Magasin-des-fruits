1f40cf50dc67dd29b8d910b9cdec89b7fc80d8df
lahlaliwassim2 <wlahlali343@gmail.com>
2023-04-27 14:33:10 +0000

    Ajouter le composant qui affiche les erreurs selon le cas d'erreur , ce dernier est passé via un props qui contient le message pour chaque cas

---
+++ src/components/cart/ErrorView.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="error">
+  <p>{{ message }}</p>
+</div>
+</template>
+<script>
+export default {
+    name:'ErrorView',
+    props : {
+        message : {
+            type: String
+        }
+    }
+}
+</script>
+<style>
+.error {
+  background-color: #f8d7da;
+  border-color: #f5c6cb;
+  color: #721c24;
+  padding: 10px;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  margin-bottom: 10px;
+}
+
+.error p {
+  margin: 0;
+  font-size: 14px;
+  line-height: 1.5;
+}
+</style>
