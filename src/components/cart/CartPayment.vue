c020c2cf2f95dcc6449bc4d9d683df5721bcd585
lahlaliwassim2 <wlahlali34@gmail.com>
2023-05-02 22:36:05 +0100

    migration ver les composant de ELEMENT UI

--- src/components/cart/CartPayment.vue
+++ src/components/cart/CartPayment.vue
@@ -1,31 +1,53 @@
 <template>
-    <div class="division">
-        <div>Le prix total : </div>
-        <div>{{prix_total}} DH</div>
-    </div>
+  <div class="division">
+    <el-popover
+      placement="top-start"
+      title="PRIX TOTAL EN EURO "
+      :width="200"
+      trigger="hover"
+      :content="popoverContent"
+    >
+      <template #reference>
+        
+        <el-button class="m-2"> Prix Total : {{prix_total}} dh</el-button>
+      </template>
+    </el-popover>
+  </div>
 </template>
+
 <script>
 export default {
   props: ['product'],
-    computed: {
-      prix_total() {
-        return  this.$store.getters.SomePrixTotal
-      }
-    }
-}
+  data() {
+    return {
+      popoverContent: this.$store.getters.SomePrixTotal/10 + " $",
+    };
+  },
+  computed: {
+    prix_total() {
+      return this.$store.getters.SomePrixTotal;
+    },
+  },
+  watch: {
+    prix_total() {
+      this.popoverContent = this.$store.getters.SomePrixTotal;
+    },
+  },
+};
 </script>
 
 <style>
   .division {
-        background-color: #eb8755;
-        color: #1f0e0e;
-        padding: 10px;
-        font-size: 20px;
-        font-weight: bold;
-        text-align: center;
-        margin-top: 126px;
-    }
-    div:first-child {
-        margin-bottom: 10px;
-    }
+    background-color: #eb8755;
+    color: #1f0e0e;
+    padding: 10px;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 126px;
+  }
+
+  div:first-child {
+    margin-bottom: 10px;
+  }
 </style>
