89bedea896b358253d41312108f6cb70b463f4f2
lahlaliwassim2 <wlahlali34@gmail.com>
2023-05-02 22:36:23 +0100

    migration ver les composant de ELEMENT UI

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -1,50 +1,51 @@
 <template>
-  <div class="cart ">
-    <BannerView 
-    titre="Mon Panier"
+  <div class="cart">
+    <el-banner
+      title="Mon Panier"
     />
-    <!-- <div v-for="product in items" :key="product.id">
-      {{ product.name }}
-    </div> -->
-    <div class="cartItem">
-    <CartItems 
-    v-for="(product) in items" 
-    :key="product.id"
-    :product="product"
-    />
-   
-</div>
-<div v-if="items.length <= 0">
-      <ErrorView 
-      :message="err "
-    />
-    </div>
-    <CartPayment 
+    <el-row v-if="items && items.length > 0">
+      <cart-item
+        v-for="product in items"
+        :key="product.id"
+        :product="product"
+      />
+    </el-row>
+    <el-row v-else>
+      <el-col :span="24">
+        <el-alert
+          title="Votre panier est vide"
+          type="warning"
+          center
+        />
+      </el-col>
+    </el-row>
+    <cart-payment
       :product="product"
     />
-    
   </div>
-
 </template>
-<script>
 
-import BannerView from '@/components/BannerView.vue';
-import CartItems from '../components/cart/CartItem.vue'
+<script>
+import { ElBanner, ElRow, ElCol, ElAlert } from 'element-plus'
+
+import CartItem from '../components/cart/CartItem.vue'
 import CartPayment from '../components/cart/CartPayment.vue'
-import ErrorView from '../components/cart/ErrorView.vue'
 
 export default {
+  components: {
+    ElBanner,
+    ElRow,
+    ElCol,
+    ElAlert,
+    CartItem, 
+    CartPayment
+  },
   data () {
     return {
       err : "Votre panier est vide"
     }
   },
-    components: {
-    CartItems,
-    CartPayment,
-    BannerView,
-    ErrorView
-},
+   
   computed: {
     items() {
       return this.$store.getters.cartItems;
@@ -58,6 +59,7 @@ export default {
     }
   }
 };
+
 </script>
 
 <style>
@@ -65,17 +67,17 @@ export default {
   display: flex;
   flex-direction: column;
 }
-.cartItem {
- display: grid;
- grid-template-columns: repeat(3, 2fr);
+.cart-item {
+  display: grid;
+  grid-template-columns: repeat(3, 2fr);
   grid-gap: 54px;
 }
 .titre {
   font-size: 25px;
 }
 @media (max-width: 500px) {
-    .cartItem {
-       display: block;
-    }
+  .cart-item {
+    display: block;
+  }
 }
 </style>
